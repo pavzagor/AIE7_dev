f8dca4f4ee32e63300d537eb555d37371ca6f164
Konstantin Valiotti <valiotti.konst@gmail.com>
2025-06-28 18:54:48 +0100

    Session 2 Homework

--- 02_Embeddings_and_RAG/aimakerspace/vectordatabase.py
+++ 02_Embeddings_and_RAG/aimakerspace/vectordatabase.py
@@ -12,11 +12,21 @@ def cosine_similarity(vector_a: np.array, vector_b: np.array) -> float:
     norm_b = np.linalg.norm(vector_b)
     return dot_product / (norm_a * norm_b)
 
+def euclidean_similarity(vector_a: np.array, vector_b: np.array) -> float:
+    """Computes the euclidean similarity between two vectors based on euclidean distance."""
+    eu_distance = np.sqrt(np.sum((vector_a - vector_b)**2))
+    return 1 / (1 + eu_distance)
 
-class VectorDatabase:
-    def __init__(self, embedding_model: EmbeddingModel = None):
+def dot_product_similarity(vector_a: np.array, vector_b: np.array) -> float:
+    """Computes the dot product similarity between two vectors."""
+    dot_product = np.dot(vector_a, vector_b)
+    return dot_product
+
+class VectorDatabase():
+    def __init__(self, embedding_model: EmbeddingModel = None, distance_measure: Callable = cosine_similarity):
         self.vectors = defaultdict(np.array)
         self.embedding_model = embedding_model or EmbeddingModel()
+        self.distance_measure = distance_measure
 
     def insert(self, key: str, vector: np.array) -> None:
         self.vectors[key] = vector
@@ -25,10 +35,9 @@ class VectorDatabase:
         self,
         query_vector: np.array,
         k: int,
-        distance_measure: Callable = cosine_similarity,
     ) -> List[Tuple[str, float]]:
         scores = [
-            (key, distance_measure(query_vector, vector))
+            (key, self.distance_measure(query_vector, vector))
             for key, vector in self.vectors.items()
         ]
         return sorted(scores, key=lambda x: x[1], reverse=True)[:k]
@@ -37,11 +46,10 @@ class VectorDatabase:
         self,
         query_text: str,
         k: int,
-        distance_measure: Callable = cosine_similarity,
         return_as_text: bool = False,
     ) -> List[Tuple[str, float]]:
         query_vector = self.embedding_model.get_embedding(query_text)
-        results = self.search(query_vector, k, distance_measure)
+        results = self.search(query_vector, k)
         return [result[0] for result in results] if return_as_text else results
 
     def retrieve_from_key(self, key: str) -> np.array:
